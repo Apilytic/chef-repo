4f70e4c8c4798bf0016808fec4ba703826f819d2
Georgi Lambov <georgi.lambov@gmail.com>
2017-01-24 23:09:29 +0200

    spec for lytc recipe.

--- cookbooks/lytc/spec/spec_helper.rb
+++ cookbooks/lytc/spec/spec_helper.rb
@@ -1,2 +1,13 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+    config.color = true
+    config.order = 'random'
+    config.filter_run_excluding broken: true
+    config.platform = 'debian'
+    config.version = '8.0'
+    config.log_level = :error
+    config.warnings = false
+    # config.formatter = 'documentation'
+end
