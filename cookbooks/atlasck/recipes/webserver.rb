76295f2b2cde8d45e79d457f125921a753341823
Georgi Lambov <georgi.lambov@gmail.com>
2017-02-02 14:38:21 +0200

    code re-format.

--- cookbooks/atlasck/recipes/webserver.rb
+++ cookbooks/atlasck/recipes/webserver.rb
@@ -7,24 +7,24 @@
 java 'install java8'
 
 tomcat_install 'atlasck' do
-    version '8.5.4'
-    install_path '/usr/local/apache-tomcat'
+  version '8.5.4'
+  install_path '/usr/local/apache-tomcat'
 end
 
 tomcat_service 'atlasck' do
-    action :enable
-    env_vars [{ 'JAVA_HOME' => '/usr/lib/jvm/java-8-oracle',
-                'CATALINA_OPTS' =>
-      '-server -Xms64m -Xmx128m -XX:MaxPermSize=96m -XX:+DisableExplicitGC' }]
+  action :enable
+  env_vars [{'JAVA_HOME' => '/usr/lib/jvm/java-8-oracle',
+             'CATALINA_OPTS' =>
+                 '-server -Xms64m -Xmx128m -XX:MaxPermSize=96m -XX:+DisableExplicitGC'}]
 end
 
 logrotate_app 'tomcat_atlasck' do
-    cookbook 'logrotate'
-    path ['/usr/local/apache-tomcat/logs/catalina.out',
-          '/usr/local/apache-tomcat/logs/*.log',
-          '/usr/local/apache-tomcat/logs/*.txt']
-    frequency 'daily'
-    options %w(missingok compress delaycompress sharedscripts notifempty)
-    rotate 30
-    create '0644 tomcat_atlasck tomcat_atlasck'
+  cookbook 'logrotate'
+  path %w(/usr/local/apache-tomcat/logs/catalina.out
+      /usr/local/apache-tomcat/logs/*.log
+      /usr/local/apache-tomcat/logs/*.txt)
+  frequency 'daily'
+  options %w(missingok compress delaycompress sharedscripts notifempty)
+  rotate 30
+  create '0644 tomcat_atlasck tomcat_atlasck'
 end
