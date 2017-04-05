92e1d347501cc60c0cac69fab1ec80aeadb840a1
gogoluxecs <gogo_luxecs@yahoo.com>
2017-04-05 23:55:03 +0300

    Added sklad1 cookbook.

---
+++ cookbooks/sklad1/README.md
@@ -0,0 +1,4 @@
+# sklad1
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/sklad1/metadata.rb
@@ -0,0 +1,15 @@
+name 'sklad1'
+maintainer 'gogoluxecs'
+maintainer_email 'georgi.lambov@gmail.com'
+license 'all_rights'
+description 'Installs/Configures sklad1'
+long_description 'Installs/Configures sklad1'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/sklad1/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/sklad1' if respond_to?(:source_url)
