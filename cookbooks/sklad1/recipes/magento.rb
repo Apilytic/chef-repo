f304ef7098db51a9be2d5321b982a0ac8e2507d4
gogoluxecs <gogo_luxecs@yahoo.com>
2017-04-06 10:30:09 +0300

    Include docker-compose recipe and provide template for Magento.

--- cookbooks/sklad1/recipes/magento.rb
+++ cookbooks/sklad1/recipes/magento.rb
@@ -4,3 +4,10 @@
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
+include_recipe 'docker_compose::installation'
+
+directory '/var/www/magento'
+
+cookbook_file '/var/www/magento/docker-compose.yml' do
+  source "magento/docker-compose.yml"
+end
