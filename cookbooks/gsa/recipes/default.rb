34bdd7e77688d7412c21834045e15de01c819c5a
gogoluxecs <gogo_luxecs@yahoo.com>
2019-01-20 22:56:50 +0200

    gsa install docker-compose.

--- cookbooks/gsa/recipes/default.rb
+++ cookbooks/gsa/recipes/default.rb
@@ -4,5 +4,13 @@
 #
 # Copyright (c) 2019 The Authors, All Rights Reserved.
 
-package 'docker'
-pacakge 'htop'
+package %w(docker curl)
+
+bash 'install docker-compose' do
+  code <<-EOH
+    curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod 0755 /usr/local/bin/docker-compose
+  EOH
+  not_if {File::exists?('/usr/local/bin/docker-compose')}
+end
+
