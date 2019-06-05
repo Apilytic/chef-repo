b764abae8daf33c036b7035c095e8aaa8bfdd4a5
gogoluxecs <gogo_luxecs@yahoo.com>
2019-06-05 09:55:35 +0300

    Recipe for attach eip.

--- cookbooks/gsa/attributes/default.rb
+++ cookbooks/gsa/attributes/default.rb
@@ -1,1 +1,2 @@
 default['gsa']['user']['name'] = 'ec2-user'
+default['gsa']['secret_path'] = '/etc/chef/encrypted_data_bag_secret'
---
+++ cookbooks/gsa/recipes/eip.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: gsa
+# Recipe:: eip
+#
+# Copyright (c) 2019 The Authors, All Rights Reserved.
+
+aws_user, aws_password = decrypt_passwords_data_bag_item(node['gsa']['secret_path'], 'gsa_codedeploy_password')
+
+bash 'attach eip' do
+  environment ({AWS_ACCESS_KEY_ID: aws_user, AWS_SECRET_ACCESS_KEY: aws_password, AWS_DEFAULT_REGION: "eu-west-1"})
+  code <<-EOH
+INSTANCE_ID=`aws autoscaling describe-auto-scaling-instances \
+  --output text --query "AutoScalingInstances[?AutoScalingGroupName=='prod'].InstanceId"`
+
+aws ec2 associate-address --instance-id $INSTANCE_ID --allocation-id eipalloc-0efee59f6261069a1
+  EOH
+end
