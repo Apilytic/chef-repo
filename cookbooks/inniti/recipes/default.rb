d8e5025c3047e64cae372d61f477240ca009560e
gogoluxecs <gogo_luxecs@yahoo.com>
2019-11-27 08:53:19 +0200

    docker setup.

--- cookbooks/inniti/recipes/default.rb
+++ cookbooks/inniti/recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright (c) 2019 The Authors, All Rights Reserved.
+
+package %w(docker.io curl)
+
+bash 'install docker-compose' do
+  code <<-EOH
+    curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod 0755 /usr/local/bin/docker-compose
+  EOH
+  not_if {File::exists?('/usr/local/bin/docker-compose')}
+end
+
+service 'docker' do
+  action :start
+end
+
+user_name = node['inniti']['user']['name']
+
+group 'docker' do
+  members user_name
+  append true
+  action :modify
+end
