2ffbbf0ae588c6d7a1647408cdf796da9b7c2247
gogoluxecs <gogo_luxecs@yahoo.com>
2019-12-02 22:50:01 +0200

    rename config prod to office.

--- cookbooks/inniti/attributes/default.rb
+++ cookbooks/inniti/attributes/default.rb
@@ -1,6 +1,6 @@
 default['inniti']['flow']['dir']['src'] = '/var/www/inniti-backend/Flow_MES'
 default['inniti']['flow']['dir']['target'] = '/home/inniti/app'
 default['inniti']['flow']['config']['path'] = '/src/resources'
-default['inniti']['flow']['config']['name'] = 'config-prod.properties'
+default['inniti']['flow']['config']['name'] = 'config-office.properties'
 default['inniti']['flow']['jar'] = 'Flow_MES-1.0-jar-with-dependencies.jar'
 default['inniti']['user']['name'] = 'inniti'
