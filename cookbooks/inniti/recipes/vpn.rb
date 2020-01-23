3e854ba46921a1c3b19af12af459e4a764a43c19
gogoluxecs <gogo_luxecs@yahoo.com>
2020-01-23 23:24:41 +0200

    chef caCert and caCey pem.

--- cookbooks/inniti/recipes/vpn.rb
+++ cookbooks/inniti/recipes/vpn.rb
@@ -12,3 +12,13 @@ vpnServerRootCer = Base64.decode64(decrypt_keys_data_bag_item(node['inniti']['se
 file '/etc/ipsec.d/cacerts/VpnServerRoot.cer' do
   content vpnServerRootCer
 end
+
+vpn_cwd = node['inniti']['vpn']['dir']['stage']
+
+directory(vpn_cwd) {recursive true}
+
+%w(caCert caKey).each do |cert|
+  file "#{vpn_cwd}/#{cert}.pem"
+  content decrypt_key_data_bag_item(node['inniti']['secret_path'].to_s, "inniti_vpn_#{cert}_key")
+end
+
