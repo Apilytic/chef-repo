4f46ad721e2dc65a8776ba0099351b095a19c4d1
gogoluxecs <gogo_luxecs@yahoo.com>
2020-03-03 00:42:26 +0200

    db_slave setup.

--- cookbooks/inniti/recipes/db_slave.rb
+++ cookbooks/inniti/recipes/db_slave.rb
@@ -3,3 +3,16 @@
 # Recipe:: db_slave
 #
 # Copyright (c) 2020 The Authors, All Rights Reserved.
+
+template '/var/www/inniti/docker/slave.cnf' do
+  source 'db_slave/slave.cnf.erb'
+  variables(server_id: node['inniti']['global_db']['server_id'])
+  user node['inniti']['user']['name']
+end
+
+template '/var/www/inniti/docker/slave_setup.sh' do
+  source 'db_slave/slave_setup.sh.erb'
+  mode 00777
+  user node['inniti']['user']['name']
+  group node['inniti']['user']['name']
+end
