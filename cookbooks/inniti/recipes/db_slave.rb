255e276bdc54871c4484fda1151b0006b4b0ab65
gogoluxecs <gogo_luxecs@yahoo.com>
2020-03-03 01:56:05 +0200

    db_slave basic change master syntax.

--- cookbooks/inniti/recipes/db_slave.rb
+++ cookbooks/inniti/recipes/db_slave.rb
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2020 The Authors, All Rights Reserved.
 
+slave_user, slave_password = decrypt_passwords_data_bag_item(secret, 'inniti_mysql_slave_password')
+
 template '/var/www/inniti/docker/slave.cnf' do
   source 'db_slave/slave.cnf.erb'
   variables(server_id: node['inniti']['global_db']['server_id'])
@@ -13,6 +15,12 @@ end
 template '/var/www/inniti/docker/slave_setup.sh' do
   source 'db_slave/slave_setup.sh.erb'
   mode 00777
+  user node['inniti']['user']['name']
+end
+
+template '/var/www/inniti/docker/change_master.sql' do
+  source 'db_slave/change_master.sql.erb'
+  variables(user: slave_user, password: slave_password)
   user node['inniti']['user']['name']
   group node['inniti']['user']['name']
 end
