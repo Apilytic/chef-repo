5d9a63af4cf05764564d7cb14bce5b48b56768ad
gogoluxecs <gogo_luxecs@yahoo.com>
2020-03-08 02:01:33 +0200

    db_master intial import for slavel

--- cookbooks/inniti/recipes/db_slave.rb
+++ cookbooks/inniti/recipes/db_slave.rb
@@ -4,7 +4,17 @@
 #
 # Copyright (c) 2020 The Authors, All Rights Reserved.
 
+db_user, db_password = decrypt_passwords_data_bag_item(secret, 'inniti_mysql_password')
 slave_user, slave_password = decrypt_passwords_data_bag_item(secret, 'inniti_mysql_slave_password')
+
+bash 'initial db dump' do
+  cwd '/var/www/inniti/docker'
+  code <<-EOH
+docker-compose exec -T mysql_app mysqldump -c --single-transaction --add-drop-table \
+--hex-blob --skip-add-drop-table inniti -u#{db_user} -p#{db_password} > inniti_slave.sql
+  EOH
+  sensitive true
+end
 
 template '/var/www/inniti/docker/slave.cnf' do
   source 'db_slave/slave.cnf.erb'
